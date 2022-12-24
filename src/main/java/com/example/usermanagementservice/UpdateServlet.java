438b069554719c4e60f828171a0adf494445d0ae
Manish V <imvmanish18@gmail.com>
2022-12-24 22:47:33 +0530

    Added updateUser servlet

--- src/main/java/com/example/usermanagementservice/UpdateServlet.java
+++ src/main/java/com/example/usermanagementservice/UpdateServlet.java
@@ -1,10 +1,8 @@
 package com.example.usermanagementservice;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,41 +11,38 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-@WebServlet("/addServlet")
-public class AddServlet extends HttpServlet {
+@WebServlet(name = "UpdateServlet", value = "/updateServlet")
+public class UpdateServlet extends HttpServlet {
 
     private Connection connection;
 
+    @Override
     public void init() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users", "root", "12345678");
-            System.out.println("DB Connection Made Successfully!");
-        } catch (SQLException e) {
+
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        }
+        System.out.println("DB Connection Made Successfully!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("UPDATE user SET password = '"+password+"' WHERE email='"+email+"'");
+            PrintWriter pw = response.getWriter();
+            pw.println("Updated password");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String firstName = request.getParameter("firstName");
-        String lastName = request.getParameter("lastName");
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        try {
-            Statement statement = connection.createStatement();
-            int result = statement.executeUpdate("INSERT INTO user VALUES('"+firstName+"','"+lastName+"','"+email+"','"+password+"')");
-            System.out.println(result + " number of rows created");
-            PrintWriter pw = response.getWriter();
-            pw.println(result + " number of rows created");
-            statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
     public void destroy() {
         try {
             connection.close();
